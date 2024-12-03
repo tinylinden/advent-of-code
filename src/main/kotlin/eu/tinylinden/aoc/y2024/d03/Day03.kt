0db7c426127f36ee9474fe9001daf512288bba42
tinylinden <lukasz.lipka@gmail.com>
2024-12-03 11:05:04 +0100

    feat: day 3, 2024

--- src/main/kotlin/eu/tinylinden/aoc/y2024/d03/Day03.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2024/d03/Day03.kt
@@ -1,12 +1,12 @@
 package eu.tinylinden.aoc.y2024.d03
 
 fun mullItOver(s: String, extended: Boolean): Int =
-    parseOps(s).fold(State(extended)) { state, op -> state.eval(op) }.result
+    parse(s).fold(State(extended)) { state, token -> state.eval(token) }.result
 
-private fun parseOps(s: String): List<String> =
+private fun parse(s: String): List<String> =
     s.lines().flatMap { line ->
-        OP.findAll(line).flatMap { it.groupValues }
-    }.also { println(it) }
+        OPS.findAll(line).flatMap { it.groupValues }
+    }
 
 private class State(
     private val extended: Boolean
@@ -14,22 +14,25 @@ private class State(
     var enabled: Boolean = true
     var result: Int = 0
 
-    fun eval(op: String): State {
+    fun eval(token: String): State {
         when {
-            op == "don't()" && extended -> enabled = false
-            op == "do()" && extended -> enabled = true
-            op.startsWith("mul") && enabled -> eval(op) { first() * last() }
+            token.matches(OP_DONT) && extended -> enabled = false
+            token.matches(OP_DO) && extended -> enabled = true
+            token.matches(OP_MUL) && enabled -> eval(token) { first() * last() }
         }
         return this
     }
 
-    private fun eval(op: String, f: Sequence<Int>.() -> Int) {
-        result += args(op).let(f)
+    private fun eval(token: String, op: Sequence<Int>.() -> Int) {
+        result += op(opArgs(token))
     }
 
-    private fun args(op: String) =
-        OP_ARGS.findAll(op).flatMap { it.groupValues }.map { it.toInt() }
+    private fun opArgs(token: String) =
+        OP_ARGS.findAll(token).flatMap { it.groupValues }.map { it.toInt() }
 }
 
-private val OP = Regex("do\\(\\)|don't\\(\\)|mul\\(\\d+,\\d+\\)")
+private val OP_DONT = Regex("don't\\(\\)")
+private val OP_DO = Regex("do\\(\\)")
+private val OP_MUL = Regex("mul\\(\\d+,\\d+\\)")
+private val OPS = Regex("$OP_DONT|$OP_DO|$OP_MUL")
 private val OP_ARGS = Regex("\\d+")
