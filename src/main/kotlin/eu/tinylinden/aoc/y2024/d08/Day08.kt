87f2915d37d230883bed288be1634df131fccc61
tinylinden <lukasz.lipka@gmail.com>
2024-12-09 11:51:15 +0100

    feat: day 8, 2024 - part two solution

--- src/main/kotlin/eu/tinylinden/aoc/y2024/d08/Day08.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2024/d08/Day08.kt
@@ -2,29 +2,39 @@ package eu.tinylinden.aoc.y2024.d08
 
 import eu.tinylinden.aoc.base.*
 
-fun resonantCollinearityOne(input: String): Int {
-    val grid = charGrid(input)
-    return grid.pointsByChar { it != '.' }
-        .filter { (_, p) -> p.size > 1 }
+fun resonantCollinearityOne(input: String): Int = countAntinodes(charGrid(input), Single)
+
+fun resonantCollinearityTwo(input: String): Int = countAntinodes(charGrid(input), Resonant)
+
+private fun countAntinodes(grid: CharGrid, f: Finder): Int =
+    grid.pointsByChar { it != '.' }
         .values
-        .flatMap { antinodes(it) }
-        .filter { grid.contains(it) }
+        .filter { it.size > 1 }
+        .flatMap { antinodes(it, grid, f) }
         .toSet()
         .size
+
+private fun antinodes(points: List<Point>, grid: CharGrid, f: Finder): List<Point> {
+    fun part(p: Point, rest: List<Point>): List<Point> =
+        when (rest.size) {
+            0 -> emptyList()
+            1 -> f(p, rest[0], grid)
+            else -> f(p, rest[0], grid) + part(p, rest.drop(1))
+        }
+
+    return points.flatMapIndexed { i, p -> part(p, points.drop(i + 1)) }
 }
 
-fun resonantCollinearityTwo(input: String): Int =
-    0
+private fun interface Finder : (Point, Point, CharGrid) -> List<Point>
 
-private fun antinodes(points: List<Point>): Set<Point> =
-    points.flatMapIndexed { i, p -> antinodes(p, points.drop(i + 1)) }.toSet()
+private val Single = Finder { l, r, grid ->
+    (r - l).let { listOf(l - it, r + it) }.filter { grid.contains(it) }
+}
 
-private fun antinodes(point: Point, rest: List<Point>): Set<Point> =
-    when (rest.size) {
-        0 -> emptySet()
-        1 -> antinodes(point, rest[0])
-        else -> antinodes(point, rest[0]) + antinodes(point, rest.drop(1))
-    }
+private val Resonant = Finder { l, r, grid ->
+    fun part(seed: Point, next: (Point) -> Point) =
+        generateSequence(seed, next).takeWhile { grid.contains(it) }.toList()
 
-private fun antinodes(l: Point, r: Point): Set<Point> =
-    (r - l).let { setOf(l - it, r + it) }
+    val delta = (r - l)
+    part(l) { it - delta } + part(r) { it + delta }
+}
