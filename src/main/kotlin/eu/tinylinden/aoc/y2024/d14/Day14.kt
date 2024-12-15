2f774b946aece1df45357b7b8bb388dc2923e92b
tinylinden <lukasz.lipka@gmail.com>
2024-12-15 23:28:14 +0100

    feat: day 14, 2024 part one solution tweaks

--- src/main/kotlin/eu/tinylinden/aoc/y2024/d14/Day14.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2024/d14/Day14.kt
@@ -4,10 +4,10 @@ import eu.tinylinden.aoc.base.parsePairs
 import eu.tinylinden.aoc.base.plus
 import eu.tinylinden.aoc.base.times
 
-fun restroomRedoubtOne(input: String, rows: Int, cols: Int): Long =
+fun restroomRedoubtOne(input: String, width: Int, height: Int): Long =
     robots(input)
-        .map { move(it, 100, rows, cols) }
-        .groupingBy { quadrant(it, rows / 2, cols / 2) }
+        .map { move(it, 100, width, height) }
+        .groupingBy { quadrant(it, width / 2, height / 2) }
         .eachCount()
         .filter { (q, _) -> q != "" }
         .values
@@ -15,33 +15,28 @@ fun restroomRedoubtOne(input: String, rows: Int, cols: Int): Long =
 
 fun restroomRedoubtTwo(input: String): Long = 0
 
-private fun move(robot: Robot, n: Int, rows: Int, cols: Int): Robot {
-    val (pos, vel) = robot
-    return (pos + vel * n).let { (row, col) ->
-        robot.copy(pos = snap(row, rows) to snap(col, cols))
-    }
+private fun move(robot: Robot, n: Int, width: Int, height: Int): Robot {
+    val (x, y) = robot.let { (pos, vel) -> pos + vel * n }
+    return robot.copy(pos = snap(x, width) to snap(y, height))
 }
 
-private fun snap(curr: Int, limbo: Int): Int =
-    when { // must be a nicer way, but i am tired
-        curr % limbo == 0 -> 0
-        curr < 0 -> limbo + (curr % limbo)
-        curr >= limbo -> curr % limbo
-        else -> curr
-    }
+private fun snap(curr: Int, limit: Int): Int = (curr % limit + limit) % limit
 
-private fun quadrant(robot: Robot, middleRow: Int, centerCol: Int): String =
-    robot.pos.let { (row, col) ->
+private fun quadrant(robot: Robot, center: Int, middle: Int): String =
+    robot.pos.let { (x, y) ->
         when {
-            row < middleRow && col < centerCol -> "NW"
-            row > middleRow && col < centerCol -> "NE"
-            row > middleRow && col > centerCol -> "SE"
-            row < middleRow && col > centerCol -> "SW"
+            x < center && y < middle -> "NW"
+            x > center && y < middle -> "NE"
+            x > center && y > middle -> "SE"
+            x < center && y > middle -> "SW"
             else -> ""
         }
     }
 
-private data class Robot(val pos: Pair<Int, Int>, val vel: Pair<Int, Int>)
+private data class Robot(
+    val pos: Pair<Int, Int>, // x, y
+    val vel: Pair<Int, Int>, // dx, dy
+)
 
 private fun robots(input: String): List<Robot> =
     parsePairs(input.replace(" ", "\n"), Regex("-*\\d+")) { it.toInt() }
