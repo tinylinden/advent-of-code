c1d3cbc304b820b340825266894e825cd14885c8
tinylinden <lukasz.lipka@gmail.com>
2025-12-03 16:24:50 +0100

    feat(2025-day-01): solution

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d01/Day01.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d01/Day01.kt
@@ -1,5 +1,52 @@
 package eu.tinylinden.aoc.y2025.d01
 
-fun secretEntranceOne(input: String): Long = 0
+import kotlin.math.abs
 
-fun secretEntranceTwo(input: String): Long = 0
+fun secretEntranceOne(input: String): Long =
+    parse(input)
+        .fold(FastDial()) { dial, clicks -> dial.rotate(clicks) }
+        .counter
+
+fun secretEntranceTwo(input: String): Long =
+    parse(input)
+        .fold(PreciseDial()) { dial, clicks -> dial.rotate(clicks) }
+        .counter
+
+private data class FastDial(
+    val position: Long = 50,
+    val counter: Long = 0,
+) {
+    fun rotate(clicks: Int): FastDial {
+        val next = position + clicks
+        return if (next % 100 == 0L) {
+            copy(position = next, counter = counter + 1)
+        } else {
+            copy(position = next)
+        }
+    }
+}
+
+private data class PreciseDial(
+    val position: Long = 50,
+    val counter: Long = 0,
+) {
+    fun rotate(clicks: Int): PreciseDial {
+        val next = position + clicks
+        return if (clicks > 0) {
+            copy(position = next, counter = counter + count(clicks, 1))
+        } else {
+            copy(position = next, counter = counter + count(clicks, -1))
+        }
+    }
+
+    private fun count(clicks: Int, direction: Int): Int =
+        generateSequence(position) { it + direction }
+            .take(abs(clicks))
+            .count { it % 100 == 0L }
+}
+
+private fun parse(input: String): List<Int> =
+    input.lines()
+        .map { line ->
+            line.drop(1).toInt() * if (line.startsWith('R')) 1 else -1
+        }
--- src/test/kotlin/eu/tinylinden/aoc/y2025/d01/Day01Test.kt
+++ src/test/kotlin/eu/tinylinden/aoc/y2025/d01/Day01Test.kt
@@ -3,6 +3,7 @@ package eu.tinylinden.aoc.y2025.d01
 import eu.tinylinden.aoc.*
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
