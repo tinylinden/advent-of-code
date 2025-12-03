585f6499e672c5fc2d7ed8e70886d350e8126e18
tinylinden <lukasz.lipka@gmail.com>
2025-12-03 16:34:15 +0100

    feat(2025-day-01): ...

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d01/Day01.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d01/Day01.kt
@@ -16,30 +16,24 @@ private data class FastDial(
     val position: Long = 50,
     val counter: Long = 0,
 ) {
-    fun rotate(clicks: Int): FastDial {
-        val next = position + clicks
-        return if (next % 100 == 0L) {
+    fun rotate(clicks: Int): FastDial = position(position + clicks)
+
+    private fun position(next: Long): FastDial =
+        if (next % 100 == 0L) {
             copy(position = next, counter = counter + 1)
         } else {
             copy(position = next)
         }
-    }
 }
 
 private data class PreciseDial(
     val position: Long = 50,
     val counter: Long = 0,
 ) {
-    fun rotate(clicks: Int): PreciseDial {
-        val next = position + clicks
-        return if (clicks > 0) {
-            copy(position = next, counter = counter + count(clicks, 1))
-        } else {
-            copy(position = next, counter = counter + count(clicks, -1))
-        }
-    }
+    fun rotate(clicks: Int): PreciseDial =
+        copy(position = position + clicks, counter = counter + count(clicks))
 
-    private fun count(clicks: Int, direction: Int): Int =
+    private fun count(clicks: Int, direction: Int = if (clicks > 0) 1 else -1): Int =
         generateSequence(position) { it + direction }
             .take(abs(clicks))
             .count { it % 100 == 0L }
