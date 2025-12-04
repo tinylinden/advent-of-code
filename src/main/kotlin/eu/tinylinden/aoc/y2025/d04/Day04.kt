701032f0536616d48a0a39b5629316c9ad4ef980
tinylinden <lukasz.lipka@gmail.com>
2025-12-04 22:23:37 +0100

    feat(2025-day-04): part one fast soluition, lists

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d04/Day04.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d04/Day04.kt
@@ -1,23 +1,16 @@
 package eu.tinylinden.aoc.y2025.d04
 
-fun printingDepartmentOne(input: String): Long {
-    val grid = parse(input)
-    val mask = mask(grid)
-    val x = mask.flatMap { it.toList() }.count { it == 0 }
-    return x.toLong()
-}
+fun printingDepartmentOne(input: String): Long =
+    mask(parse(input)).flatten().count { it == 0 }.toLong()
 
 fun printingDepartmentTwo(input: String): Long = 0
 
-private typealias Grid = Array<IntArray>
+private typealias Grid = List<List<Int>>
 
-private fun mask(grid: Grid): Grid {
-    val cn = grid.mapIndexed { ri, row ->
+private fun mask(grid: Grid): Grid =
+    grid.mapIndexed { ri, row ->
         row.mapIndexed { ci, _ -> mask(grid, ri, ci) }
-            .toIntArray()
     }
-    return cn.toTypedArray()
-}
 
 private fun mask(grid: Grid, ri: Int, ci: Int, thr: Int = 4): Int =
     if (cell(grid, ri, ci) == 0) {
@@ -39,12 +32,9 @@ private fun mask(grid: Grid, ri: Int, ci: Int, thr: Int = 4): Int =
 private fun cell(grid: Grid, ri: Int, ci: Int): Int =
     runCatching { grid[ri][ci] }.getOrDefault(0)
 
-private fun parse(input: String): Grid {
-    val parsed = input.lines()
+private fun parse(input: String): Grid =
+    input.lines()
         .map { line ->
             line.toCharArray()
                 .map { chr -> if (chr == '.') 0 else 1 }
-                .toIntArray()
         }
-    return parsed.toTypedArray()
-}
