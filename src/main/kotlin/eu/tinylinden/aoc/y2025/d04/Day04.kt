643b50e9bec60ef6ccf82f8440f68fbc2482b389
tinylinden <lukasz.lipka@gmail.com>
2025-12-04 22:18:23 +0100

    feat(2025-day-04): part one fast soluition, arrays

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d04/Day04.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d04/Day04.kt
@@ -2,45 +2,49 @@ package eu.tinylinden.aoc.y2025.d04
 
 fun printingDepartmentOne(input: String): Long {
     val grid = parse(input)
-    return grid.map { roll -> countAdjacent(roll, grid) }
-        .count { it < 4 }
-        .toLong()
+    val mask = mask(grid)
+    val x = mask.flatMap { it.toList() }.count { it == 0 }
+    return x.toLong()
 }
 
 fun printingDepartmentTwo(input: String): Long = 0
 
-private typealias Roll = Pair<Int, Int>
-private typealias Grid = Set<Roll>
+private typealias Grid = Array<IntArray>
 
-private fun countAdjacent(roll: Roll, grid: Grid): Int =
-    (grid intersect adjacent(roll)).size
-
-private fun adjacent(roll: Roll): Grid {
-    val (x, y) = roll
-    return setOf(
-        x - 1 to y - 1,
-        x to y - 1,
-        x + 1 to y - 1,
-        x - 1 to y,
-        x + 1 to y,
-        x - 1 to y + 1,
-        x to y + 1,
-        x + 1 to y + 1,
-    )
+private fun mask(grid: Grid): Grid {
+    val cn = grid.mapIndexed { ri, row ->
+        row.mapIndexed { ci, _ -> mask(grid, ri, ci) }
+            .toIntArray()
+    }
+    return cn.toTypedArray()
 }
 
-private fun parse(input: String): Grid =
-    input.lines()
-        .flatMapIndexed { y, line -> parseLine(y, line) }
-        .filterNotNull()
-        .toSet()
+private fun mask(grid: Grid, ri: Int, ci: Int, thr: Int = 4): Int =
+    if (cell(grid, ri, ci) == 0) {
+        1
+    } else {
+        // @formatter:off
+        val adj = cell(grid, ri - 1, ci - 1) + // NW
+                  cell(grid, ri - 1, ci    ) + // N
+                  cell(grid, ri - 1, ci + 1) + // NE
+                  cell(grid, ri    , ci + 1) + // E
+                  cell(grid, ri + 1, ci + 1) + // SE
+                  cell(grid, ri + 1, ci    ) + // S
+                  cell(grid, ri + 1, ci - 1) + // SW
+                  cell(grid, ri    , ci - 1)   // W
+        // @formatter:on
+        if (adj < thr) 0 else 1
+    }
 
-private fun parseLine(y: Int, line: String): List<Roll?> =
-    line.toCharArray()
-        .mapIndexed { x, chr ->
-            if (chr == '.') {
-                null
-            } else {
-                x to y
-            }
+private fun cell(grid: Grid, ri: Int, ci: Int): Int =
+    runCatching { grid[ri][ci] }.getOrDefault(0)
+
+private fun parse(input: String): Grid {
+    val parsed = input.lines()
+        .map { line ->
+            line.toCharArray()
+                .map { chr -> if (chr == '.') 0 else 1 }
+                .toIntArray()
         }
+    return parsed.toTypedArray()
+}
