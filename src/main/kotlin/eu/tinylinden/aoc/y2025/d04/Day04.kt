8e95ac861851d7d8fe33ee15cbdcbc15278655d1
tinylinden <lukasz.lipka@gmail.com>
2025-12-04 13:19:21 +0100

    feat(2025-day-04): scaffolding

---
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d04/Day04.kt
@@ -0,0 +1,9 @@
+package eu.tinylinden.aoc.y2025.d04
+
+fun printingDepartmentOne(input: String): Long = 0
+
+fun printingDepartmentTwo(input: String): Long = 0
+
+private typealias Grid = Set<Pair<Int, Int>>
+
+private fun parse(input: String): Grid = emptySet()
