b5dea9890a9be13cbea8653337d306f67cf67d71
Łukasz Lipka <tinylinden@users.noreply.github.com>
2025-12-04 13:05:24 +0100

    feat(2025-day-03): solution

---
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d03/Day03.kt
@@ -0,0 +1,31 @@
+package eu.tinylinden.aoc.y2025.d03
+
+fun lobbyOne(input: String): Long = solve(input, 2)
+
+fun lobbyTwo(input: String): Long = solve(input, 12)
+
+private typealias Battery = Int
+private typealias BatteryBank = List<Battery>
+
+private fun solve(input: String, size: Int): Long =
+    parse(input)
+        .map { bank -> joltage(bank, size, emptyList()) }
+        .sumOf { it.joinToLong() }
+
+private tailrec fun joltage(bank: BatteryBank, size: Int, acc: List<Int>): List<Int> =
+    if (acc.size == size) {
+        acc
+    } else {
+        val (idx, vol) = bank.dropLast(size - acc.size - 1) // keep enough batteries
+            .mapIndexed { i, v -> i to v }                      // index of max vol will be handy
+            .maxBy { (_, v) -> v }
+        joltage(bank.drop(idx + 1), size, acc + vol)
+    }
+
+private fun List<Int>.joinToLong(): Long = joinToString(separator = "").toLong()
+
+private fun parse(input: String): List<BatteryBank> =
+    input.lines()
+        .map { line ->
+            line.toCharArray().map { it.digitToInt() }
+        }
