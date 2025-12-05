f27d3fd61bf8c43a68827e1c27c29a74d57ce511
tinylinden <lukasz.lipka@gmail.com>
2025-12-05 21:40:07 +0100

    feat(2025-day-02): solution part one

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d02/Day02.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d02/Day02.kt
@@ -1,5 +1,20 @@
 package eu.tinylinden.aoc.y2025.d02
 
-fun giftShopOne(input: String): Long = 0
+fun giftShopOne(input: String): Long =
+    parse(input)
+        .flatMap { invalid(it) }
+        .sum()
 
 fun giftShopTwo(input: String): Long = 0
+
+private fun invalid(range: LongRange): List<Long> {
+    fun invalid(s: String): Boolean =
+        s.length % 2 == 0 && s.chunked(s.length / 2).let { it.first() == it.last() }
+
+    return range.filter { invalid("$it") }
+}
+
+private fun parse(input: String): List<LongRange> =
+    input.split(',')
+        .map { it.split('-') }
+        .map { it.first().toLong()..it.last().toLong() }
