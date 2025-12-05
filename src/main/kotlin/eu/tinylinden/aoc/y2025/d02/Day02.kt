0fe20ea62f8fdec708411bba49b731d2b0123395
tinylinden <lukasz.lipka@gmail.com>
2025-12-05 21:46:54 +0100

    feat(2025-day-02): clean-up part one

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d02/Day02.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d02/Day02.kt
@@ -2,17 +2,13 @@ package eu.tinylinden.aoc.y2025.d02
 
 fun giftShopOne(input: String): Long =
     parse(input)
-        .flatMap { invalid(it) }
+        .flatMap { range -> range.filter { invalidOne("$it") } }
         .sum()
 
 fun giftShopTwo(input: String): Long = 0
 
-private fun invalid(range: LongRange): List<Long> {
-    fun invalid(s: String): Boolean =
-        s.length % 2 == 0 && s.chunked(s.length / 2).let { it.first() == it.last() }
-
-    return range.filter { invalid("$it") }
-}
+private fun invalidOne(s: String): Boolean =
+    s.length % 2 == 0 && s.chunked(s.length / 2).let { it.first() == it.last() }
 
 private fun parse(input: String): List<LongRange> =
     input.split(',')
