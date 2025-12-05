f48b9acbb5083a731867ce1c778506aed038726d
tinylinden <lukasz.lipka@gmail.com>
2025-12-05 21:55:36 +0100

    feat(2025-day-02): solution part two, slow

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d02/Day02.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d02/Day02.kt
@@ -5,10 +5,19 @@ fun giftShopOne(input: String): Long =
         .flatMap { range -> range.filter { invalidOne("$it") } }
         .sum()
 
-fun giftShopTwo(input: String): Long = 0
+fun giftShopTwo(input: String): Long =
+    parse(input)
+        .flatMap { range -> range.filter { invalidTwo("$it") } }
+        .sum()
 
 private fun invalidOne(s: String): Boolean =
     s.length % 2 == 0 && s.chunked(s.length / 2).let { it.first() == it.last() }
+
+// slow but working
+private fun invalidTwo(s: String): Boolean =
+    generateSequence(1) { it + 1 }
+        .take(s.length - 1)
+        .any { s.chunked(it).distinct().size == 1 }
 
 private fun parse(input: String): List<LongRange> =
     input.split(',')
