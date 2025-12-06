83254fd75d66c118ae0d01678d9b8512d1f8ee28
tinylinden <lukasz.lipka@gmail.com>
2025-12-06 12:41:45 +0100

    feat(2025-day-06): solution part one

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d06/Day06.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d06/Day06.kt
@@ -1,5 +1,28 @@
 package eu.tinylinden.aoc.y2025.d06
 
-fun trashCompactorOne(input: String): Long = 0
+fun trashCompactorOne(input: String): Long =
+    parse(input).sumOf { it.solution() }
 
 fun trashCompactorTwo(input: String): Long = 0
+
+private data class Problem(
+    val numbers: List<Long>,
+    val symbol: String,
+) {
+    fun solution(): Long =
+        if (symbol == "+") {
+            numbers.fold(0L) { l, r -> l + r }
+        } else { // *
+            numbers.fold(1L) { l, r -> l * r }
+        }
+}
+
+private fun parse(input: String): List<Problem> {
+    val (symbols, numbers) = input.lines()
+        .map { it.trim().split(Regex("\\s+")) }
+        .let { it.last() to it.dropLast(1) }
+
+    return symbols.mapIndexed { idx, symbol ->
+        Problem(numbers.map { it[idx].toLong() }, symbol)
+    }
+}
