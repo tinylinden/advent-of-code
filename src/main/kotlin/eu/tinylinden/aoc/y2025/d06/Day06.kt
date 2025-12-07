4fc9dcc2ab1448a99cd237439c2dcb0dd7a67db4
tinylinden <lukasz.lipka@gmail.com>
2025-12-07 10:19:09 +0100

    style: test only current edition in ci build

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d06/Day06.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d06/Day06.kt
@@ -1,41 +1,40 @@
 package eu.tinylinden.aoc.y2025.d06
 
 fun trashCompactorOne(input: String): Long =
-    parseColumns(input).sumOf { it.answer() }
+    parse(input).sumOf { it.answer() }
 
 fun trashCompactorTwo(input: String): Long =
-    parseColumns(input).sumOf { it.rotate().answer() }
+    parse(input).sumOf { it.rotate().answer() }
 
-data class Column(
-    val numbers: List<String>,
+data class Problem(
+    val numbers: List<String>, // spaces preserved as they have meaning for rotating (reading numbers vertically)
     val symbol: Char,
 ) {
     fun answer(): Long =
         numbers.map { it.trim().toLong() }
             .let { if (symbol == '+') it.sum() else it.reduce { l, r -> l * r } }
 
-
-    fun rotate(): Column =
+    fun rotate(): Problem =
         numbers.first()
-            .mapIndexed { idx, _ -> numbers.map { it[idx] }.joinToString("") }
+            .mapIndexed { i, _ -> numbers.map { it[i] }.joinToString("") }
             .let { copy(numbers = it) }
 }
 
-private fun parseColumns(input: String): List<Column> {
-    tailrec fun part(l: Int, r: Int, symbols: String, numbers: List<String>, acc: List<Column>): List<Column> {
-        fun column() = Column(numbers.map { it.substring(l, r - 1) }, symbols[l])
+private fun parse(input: String): List<Problem> {
+    tailrec fun part(l: Int, r: Int, symbols: String, numbers: List<String>, acc: List<Problem>): List<Problem> {
+        fun problem() = Problem(numbers.map { it.substring(l, r - 1) }, symbols[l])
 
         return when {
             l == -1 -> acc
             symbols[l] == ' ' -> part(l - 1, r, symbols, numbers, acc)
-            else -> part(l - 1, l, symbols, numbers, acc + column())
+            else -> part(l - 1, l, symbols, numbers, acc + problem())
         }
     }
 
     val lines = input.lines()
-    val width = lines.maxOf { it.length } + 1 // compensate missing ' ' after last column
+    val width = lines.maxOf { it.length } + 1 // compensate missing space for last column
 
     return lines
-        .map { it.padEnd(width, ' ') } // restore end ' '-es - could be trimmed while ^c^v
+        .map { it.padEnd(width, ' ') } // trailing spaces can be trimmed while ^c^V
         .let { part(width - 1, width, it.last(), it.dropLast(1), emptyList()) }
 }
