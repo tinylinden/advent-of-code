b95791a8ebf88f09f29ce30fe3b115b5d8943af8
tinylinden <lukasz.lipka@gmail.com>
2025-12-09 17:19:48 +0100

    feat(2025-day-09): some clean-up

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d09/Day09.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d09/Day09.kt
@@ -4,19 +4,17 @@ import kotlin.math.abs
 
 fun movieTheaterOne(input: String): Long {
     val graph = Graph(parse(input))
-    return graph.edges.maxOf { (l, r) -> area(l, r) }
+    return graph.edges.maxOf { it.third }
 }
 
-fun movieTheaterTwo(input: String): Long = 0
+fun movieTheaterTwo(input: String): Long {
+    val reds = Graph(parse(input))
+
+    return 0
+}
 
 private typealias Vertex = Pair<Long, Long>
-private typealias Edge = Pair<Vertex, Vertex>
-
-private fun area(left: Vertex, right: Vertex): Long {
-    val (lx, ly) = left
-    val (rx, ry) = right
-    return (abs(lx - rx) + 1) * (abs(ly - ry) + 1)
-}
+private typealias Edge = Triple<Vertex, Vertex, Long>
 
 private class Graph(
     val vertices: List<Vertex>
@@ -27,10 +25,17 @@ private class Graph(
         if (idx == vertices.size) {
             acc
         } else {
-            val part = vertices.subList(idx + 1, vertices.size).map { Edge(vertices[idx], it) }
+            val part = vertices.subList(idx + 1, vertices.size).map { edge(vertices[idx], it) }
             acc.addAll(part)
             edges(idx + 1, acc)
         }
+}
+
+private fun edge(left: Vertex, right: Vertex): Edge {
+    val (lx, ly) = left
+    val (rx, ry) = right
+    val area = (abs(lx - rx) + 1) * (abs(ly - ry) + 1)
+    return Edge(left, right, area)
 }
 
 private fun parse(input: String): List<Pair<Long, Long>> =
