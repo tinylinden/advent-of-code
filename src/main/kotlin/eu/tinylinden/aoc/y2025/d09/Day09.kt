e056f3bc3d18fcdf68f628b1cd39d61c18fcb505
tinylinden <lukasz.lipka@gmail.com>
2025-12-11 12:49:52 +0100

    feat(2025-day-09): solution part two, brute-force optimization

--- src/main/kotlin/eu/tinylinden/aoc/y2025/d09/Day09.kt
+++ src/main/kotlin/eu/tinylinden/aoc/y2025/d09/Day09.kt
@@ -1,7 +1,8 @@
 package eu.tinylinden.aoc.y2025.d09
 
 import eu.tinylinden.aoc.base.graphs.SimpleWeightedGraph
-import org.locationtech.jts.geom.Geometry
+import org.locationtech.jts.geom.Polygonal
+import org.locationtech.jts.geom.prep.PreparedPolygon
 import org.locationtech.jts.io.WKTReader
 import kotlin.math.abs
 import kotlin.math.max
@@ -17,24 +18,48 @@ fun movieTheaterTwo(input: String): Long {
 }
 
 private typealias Tile = Pair<Long, Long>
-private typealias Rectangle = Triple<Tile, Tile, Long>
 
-// no time to invent 2d geometry
 private fun redOrGreenOnly(graph: SimpleWeightedGraph<Tile, Long>, reader: WKTReader = WKTReader()): Long {
-    fun geometry(rectangle: Rectangle): Geometry {
-        val (xl, yl) = rectangle.first
-        val (xr, yr) = rectangle.second
-        return reader.read("POLYGON (($xl $yl, $xl $yr, $xr $yr, $xr $yl, $xl $yl))")
-    }
-
-    val polygon = (graph.vertices + graph.vertices.first())
+    val poly = (graph.vertices + graph.vertices.first())
         .joinToString(prefix = "POLYGON ((", postfix = "))") { (x, y) -> "$x $y" }
-        .let { reader.read(it) }
+        .let { PreparedPolygon(reader.read(it) as Polygonal) }
+
+    val idx = idx(graph)
 
     return graph.edges
         .parallelStream()
-        .filter { polygon.covers(geometry(it)) }
+        .filter { (left, right) ->
+            if (left in idx || right in idx) {
+                val (xl, yl) = left
+                val (xr, yr) = right
+                val rect = reader.read("POLYGON (($xl $yl, $xl $yr, $xr $yr, $xr $yl, $xl $yl))")
+                poly.contains(rect)
+            } else {
+                false
+            }
+        }
         .reduce(0L, { acc, (_, _, c) -> max(acc, c) }, { l, r -> max(l, r) })
+}
+
+// meaningless fun name, but should return set of tiles on two longest
+// horizontal edges of polygon defined by graph vertices. why? because
+// puzzle input, that's why
+private fun idx(graph: SimpleWeightedGraph<Tile, *>): Set<Tile> {
+    // also - example input starts with horizontal edge, and puzzle input
+    // starts with vertical one
+    val polyEdges =
+        if (graph.vertices[0].first == graph.vertices[1].first) {
+            graph.vertices.drop(1) + graph.vertices[0]
+        } else {
+            graph.vertices
+        }
+
+    return polyEdges.asSequence()
+        .chunked(2)
+        .sortedByDescending { abs(it[0].first - it[1].first) }
+        .take(2)
+        .flatten()
+        .toSet()
 }
 
 private val area: (Tile, Tile) -> Long = { left, right ->
